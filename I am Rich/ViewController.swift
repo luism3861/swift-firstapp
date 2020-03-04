249a9d5be92e1c11fb39a3f583b3c3667b94e465
user165808 <user165808@74-80-250-231.ic2net.net>
2020-03-03 20:31:55 -0800

    add my first commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by user165808 on 3/2/20.
+//  Copyright © 2020 user165808. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
+
